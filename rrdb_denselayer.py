39aeea7f61c1989958fff778bf54839355ace75a
NAVIEGUY <1797328835qq.com>
2025-05-27 15:33:52 +0800

    rrdb_CAFAM

--- rrdb_denselayer.py
+++ rrdb_denselayer.py
@@ -7,45 +7,24 @@ import functools
 import config as c
 
 
-# Dense connection
-# class ResidualDenseBlock_out(nn.Module):
-#     def __init__(self, input, output, nf=c.nf, gc=c.gc, bias=True, use_snorm=False):
-#         super(ResidualDenseBlock_out, self).__init__()
-#         # gc: growth channel, i.e. intermediate channels
-#         if use_snorm:
-#             self.conv1 = nn.utils.spectral_norm(nn.Conv2d(nf, gc, 3, 1, 1, bias=bias))
-#             self.conv2 = nn.utils.spectral_norm(nn.Conv2d(nf + gc, gc, 3, 1, 1, bias=bias))
-#             self.conv3 = nn.utils.spectral_norm(nn.Conv2d(nf + 2 * gc, gc, 3, 1, 1, bias=bias))
-#             self.conv4 = nn.utils.spectral_norm(nn.Conv2d(nf + 3 * gc, gc, 3, 1, 1, bias=bias))
-#             self.conv5 = nn.utils.spectral_norm(nn.Conv2d(nf + 4 * gc, output, 3, 1, 1, bias=bias))
-#         else:
-#             self.conv1 = nn.Conv2d(input, 32, 3, 1, 1, bias=bias)
-#             self.conv2 = nn.Conv2d(input + 32, 32, 3, 1, 1, bias=bias)
-#             self.conv3 = nn.Conv2d(input + 2 * 32, 32, 3, 1, 1, bias=bias)
-#             self.conv4 = nn.Conv2d(input + 3 * 32, 32, 3, 1, 1, bias=bias)
-#             self.conv5 = nn.Conv2d(input + 4 * 32, output, 3, 1, 1, bias=bias)
-#         self.lrelu = nn.LeakyReLU(inplace=True)
-#         # initialization
-#         # mutil.initialize_weights([self.conv1, self.conv2, self.conv3, self.conv4, self.conv5], 0.1)
-#         mutil.initialize_weights([self.conv5], 0.)
-#         # self.BN1 = nn.BatchNorm2d(32)
-#         #
-#         # self.BN2 = nn.BatchNorm2d(32)
-#         #
-#         # self.BN3 = nn.BatchNorm2d(32)
-#         #
-#         # self.BN4 = nn.BatchNorm2d(32)
 
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
 
+        self.fc1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
 
-    # def forward(self, x):
-    #     x1 = self.lrelu(self.conv1(x))
-    #     x2 = self.lrelu(self.conv2(torch.cat((x, x1), 1)))
-    #     x3 = self.lrelu(self.conv3(torch.cat((x, x1, x2), 1)))
-    #     x4 = self.lrelu(self.conv4(torch.cat((x, x1, x2, x3), 1)))
-    #     x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
-    #     return x5
+        self.sigmoid = nn.Sigmoid()
 
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
 
 class ResidualDenseBlock_out(nn.Module):
     def __init__(self, input, output, nf=c.nf, gc=c.gc, bias=True, use_snorm=False):
@@ -58,17 +37,16 @@ class ResidualDenseBlock_out(nn.Module):
         self.conv4 = nn.Conv2d(32, 32, 3, 1, 1, bias=bias)
         self.conv5 = nn.Conv2d(32 * 4 + input, output, kernel_size=1, padding=0, stride=1, bias=bias)
         self.lrelu = nn.LeakyReLU(inplace=True)
-
+        self.attention = ChannelAttention(32*4+input)
         mutil.initialize_weights([self.conv5], 0.)
-
-
-
 
     def forward(self, x):
         x1 = self.lrelu(self.conv1(x))
         x2 = self.lrelu(self.conv2(x1))
         x3 = self.lrelu(self.conv3(x2))
         x4 = self.lrelu(self.conv4(x3))
-        out = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
-
+        x5 = torch.cat((x, x1, x2, x3, x4),1)
+        x6 = self.attention(x5) * x5
+        #out = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
+        out = self.conv5(x6)
         return out
